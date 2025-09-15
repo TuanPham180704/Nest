83b98ad1fe395d47a04be43980a1eb1ee2f7c4a3
Tuandev18 <phamtuan180704@gmail.com>
2025-09-15 14:50:48 +0700

    CRUD USER

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,3 +1,10 @@
-export class CreateUserDto {
-    
-}
+  import {IsString,IsEmail,IsNotEmpty} from 'class-validator'
+  export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsEmail()
+    email: string; 
+
+  }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: Array<CreateUserDto & { id: number }> = []; 
+
+  create(createUserDto: CreateUserDto) {
+    const newUser = { id: Date.now(), ...createUserDto };
+    this.users.push(newUser);
+    return newUser;
+  }
+  
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return this.users.find(user => user.id === id);
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    const userIndex = this.users.findIndex(user => user.id === id);
+    if (userIndex === -1) return null;
+    this.users[userIndex] = { ...this.users[userIndex], ...updateUserDto };
+    return this.users[userIndex];
+  }
+
+  remove(id: number) {
+    const userIndex = this.users.findIndex(user => user.id === id);
+    if (userIndex === -1) return null;
+    const deletedUser = this.users[userIndex];
+    this.users.splice(userIndex, 1);
+    return deletedUser;
+  }
+}
